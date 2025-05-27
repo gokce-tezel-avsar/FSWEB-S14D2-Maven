93c30210780a57b923d5f358f515de8c56fc3100
gokce-tezel-avsar <gokcetezelavsar@gmail.com>
2025-05-28 01:27:13 +0300

    ..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import org.example.model.Bedroom;
+import org.example.model.*;
 import org.example.model.enums.LampType;
 import org.example.model.enums.PaintColor;
 
 public class Main {
     public static void main(String[] args) {
-        Bedroom bedroom = new Bedroom();
+        Bedroom bedroom = new Bedroom("Gökçe",new Wall("N"), new Wall("S"),new Wall("W"), new Wall("E"), new Ceiling(2,PaintColor.GREEN),new Lamp(100,true,LampType.NEON),new Carpet(300,400,PaintColor.RED));
     }
 }
--- src/main/java/org/example/model/Bedroom.java
+++ src/main/java/org/example/model/Bedroom.java
@@ -1,8 +1,8 @@
 package org.example.model;
 
 public class Bedroom extends Room{
-    private Bed bed;
-    private Wardrobe wardrobe;
+    private Bed bed;//composition
+    private Wardrobe wardrobe; //composition
 
     public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4,Ceiling ceiling,Lamp lamp,Carpet carpet){
         super(name,wall1,wall2,wall3,wall4, ceiling, lamp, carpet);
